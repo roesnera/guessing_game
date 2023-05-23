5e1a5e3a05e5a581efb403e29f34594ffed23840
roesnera <adam.roesner@generation.org>
2023-05-23 13:15:38 -0400

    adds looping until successful guess

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,28 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("Please enter a number between 1-100: ");
-    let mut number: String = String::new();
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    io::stdin().read_line(&mut number).expect("Number must between 1-100");
+    // io::stdin().read_line(&mut number).expect("Number must between 1-100");
 
-    println!("You guessed: {number}");
+    // println!("You guessed: {number}");
 
-    let guess: i32 = number.trim().parse().expect("Must be a number!");
+    loop {
+        println!("Please enter a number between 1-100: ");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!")
+        // number has to be redeclared each loop iteration or program will crash on parse
+        let mut number: String = String::new();
+        io::stdin().read_line(&mut number).expect("Number must between 1-100");
+        let guess: i32 = number.trim().parse().expect("Must be a number!");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 
-    
+
 }
